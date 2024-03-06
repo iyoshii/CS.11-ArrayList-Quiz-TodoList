72d8db0bebcd061690b17a79fb867e313ac2d3d2
yoshi <puggiedountsme@gmail.com>
2024-03-06 08:11:51 +0800

    Update 1.0

--- src/TodoList.java
+++ src/TodoList.java
@@ -11,7 +11,7 @@ public class TodoList {
     }
 
     public boolean addTask(String name, int urgency) {
-        for (Task task: tasks) {
+        for (Task task : tasks) {
             if (task.getName().equals(name))
                 return false;
         }
@@ -44,12 +44,19 @@ public class TodoList {
      * The method mostUrgent will return the name of the task that is the most urgent (i.e. the task with the
      * highest urgency). If there are multiple tasks whose urgency is equal to the maximum, the first task
      * with that urgency will be returned.
+     *
      * @return the name of the task with the current highest urgency (a String).
      */
     public String mostUrgent() {
+        Task mostUrgentTask = tasks.get(0);
 
-        return null;
+        for (Task task : tasks) {
+            if (task.getUrgency() > mostUrgentTask.getUrgency()) {
+                mostUrgentTask = task;
+            }
+        }
 
+        return mostUrgentTask.getName();
     }
 
     /**
@@ -57,9 +64,13 @@ public class TodoList {
      * @return the average urgency across all tasks (a double).
      */
     public double averageUrgency() {
+        int totalUrgency = 0;
 
-        return 0.0;
+        for (Task task : tasks) {
+            totalUrgency += task.getUrgency();
+        }
 
+        return (double) totalUrgency / tasks.size();
     }
 
     /**
@@ -77,9 +88,7 @@ public class TodoList {
      * BONUS Challenge:  Sort your to-do list in descending order of urgency
      */
     public String toString() {
-
         return "";
-        
     }
 
 }
